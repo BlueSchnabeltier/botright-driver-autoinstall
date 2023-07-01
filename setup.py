b3e629d7158806019c3e86957c3ab0948e6b786d
BlueSchnabeltier <97361820+BlueSchnabeltier@users.noreply.github.com>
2023-07-02 00:39:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="botright-driver-autoinstall", install_requires=["botright", "playwright-driver-autoinstall@git+https://github.com/BlueSchnabeltier/playwright-driver-autoinstall.git")
+setup(name="botright-driver-autoinstall", install_requires=["botright", "playwright-driver-autoinstall@git+https://github.com/BlueSchnabeltier/playwright-driver-autoinstall.git"])
